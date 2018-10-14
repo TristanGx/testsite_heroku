e954acb3bb31b340509f1aacc08dc0331ba64619
Tristan <trra53@gmail.com>
2018-10-14 18:36:43 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.set("port", (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + "/public"))
+
+app.set("views", __dirname = "/views");
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-    res.sendFile("src/index.html");
-}).listen(80)
+    res.render("index.html")
+});
+
+app.listen(app.get("port"), function () {
+    console.log("listening at: " + app.get("port"));
+});
