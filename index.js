e21647314493fc8195f0085063da7526c2384264
Tristan <trra53@gmail.com>
2018-10-15 18:51:45 +0800

    last?

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const app = express();
 
 app.set("port", (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + "/public"))
-
-app.set("views", __dirname + "/views");
-
 app.get("/", (req, res) => {
-    console.log(__dirname)
+    res.sendFile(__dirname + "/views/index.html")
 });
 
 app.listen(app.get("port"), function () {
